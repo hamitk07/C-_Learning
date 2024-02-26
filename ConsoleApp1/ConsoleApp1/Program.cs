18aea403ad189e628ece2cc09c029f5c81c2c709
Hamit Karakoyunlu <hmtkrkynl@gmail.com>
2024-02-26 15:54:02 +0300

    Prime_Number_Check
    In this project, you can check whether the number you enter is a prime number.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Sayı girin:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; number > i; i++)
+{
+    if (number % i == 0)
+    {
+        Console.WriteLine("Asal değil");
+        break;
+    }
+    else if (i == number - 1)
+    {
+        Console.WriteLine("Asal");
+    }
+}
